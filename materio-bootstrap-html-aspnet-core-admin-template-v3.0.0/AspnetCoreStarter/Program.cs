c98dc2ea4713474422d79e378f4e02ee598b941a
Davi Silva <davihs4005m@gmail.com>
2026-03-04 10:28:15 +0000

    Program.cs CORRIGIDO (com Sessões Ativas)

--- materio-bootstrap-html-aspnet-core-admin-template-v3.0.0/AspnetCoreStarter/Program.cs
+++ materio-bootstrap-html-aspnet-core-admin-template-v3.0.0/AspnetCoreStarter/Program.cs
@@ -1,43 +1,33 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-// using AspnetCoreStarter.Data;
-// using AspnetCoreStarter.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-// Add services to the container.
-// During development we enable runtime compilation so changes to .cshtml files
-// are picked up without restarting dotnet. You'll need to run
-//    dotnet add package Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation
-// once to install the package.
+// 🔐 ATIVAR SESSÕES
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // sessão expira em 30 minutos
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+// Razor Pages
 #if DEBUG
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 #else
 builder.Services.AddRazorPages();
 #endif
 
-// builder.Services.AddDbContext<UserContext>(options =>
-// {
-//   options.UseSqlite(builder.Configuration.GetConnectionString("UserContext") ?? throw new InvalidOperationException("Connection string 'UserContext' not found."));
-// }, ServiceLifetime.Scoped);
-
 var app = builder.Build();
 
-// using (var scope = app.Services.CreateScope())
-// {
-//   var services = scope.ServiceProvider;
-
-//   SeedData.Initialize(services);
-// }
-
-// Configure the HTTP request pipeline.
+// Pipeline
 if (!app.Environment.IsDevelopment())
 {
-  app.UseExceptionHandler("/Error");
-  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-  app.UseHsts();
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -45,13 +35,16 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// 🔐 USAR SESSÃO (IMPORTANTE: antes de Authorization)
+app.UseSession();
+
 app.UseAuthorization();
 
-// redirect root URL to landing page
+// Redirecionar root para LandingPage
 app.MapGet("/", context =>
 {
-    context.Response.Redirect("/frontpages/Landingpage");
-    return System.Threading.Tasks.Task.CompletedTask;
+    context.Response.Redirect("/FrontPages/LandingPage");
+    return Task.CompletedTask;
 });
 
 app.MapRazorPages();
